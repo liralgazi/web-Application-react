460568493f11f680c071378e1de4dbcafec0f649
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2022-11-16 20:42:20 +0200

    create new react app

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+const products = [
+    {product: "apple", price:"30" ,image:"/images/apple" }
+]
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="productRow"></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
