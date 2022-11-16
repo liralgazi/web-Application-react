5cfb4194a41b626052d4657915add36cfac9755b
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2022-11-16 21:31:00 +0200

    add products

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Product from "./components/Product.js";
 
 
-const products = [
-    {product: "apple", price:"30" ,image:"/images/apple" }
+const items = [
+    {id:"1", product: "apple", price:"30" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=" }
 ]
 
 function App() {
@@ -10,6 +11,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="productRow"></div>
+          {
+              items.map(item=><Product key={item.id} product={item.product}
+                                       price={item.price}
+                                       image={item.image}/>)
+          }
       </header>
     </div>
   );
--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -1,2 +1,14 @@
-const Product=(props)=>{
+import './product.css'
+
+const Product=({product,price,image})=>{
+    console.log(image);
+    return (
+        <div className='product-item'>
+            <div className="product__item__description "> {product}</div>
+            <div className="product__item__price"> {price}</div>
+            <img src={image} className="product__item__image" alt={"shir"}/>
+        </div>
+    );
 }
+
+export default Product;
