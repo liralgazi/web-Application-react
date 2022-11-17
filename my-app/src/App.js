bcdfc9d8884ce23daed2e9835617e0e641c968a8
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2022-11-17 22:49:14 +0200

    add style to product

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,25 @@ import Product from "./components/Product.js";
 
 
 const items = [
-    {id:"1", product: "apple", price:"30" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=" }
+    {id:"1", product: "Red Apple", price:"30 per kg" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=",},
+    {id:"2", product: "Green Apple", price:"20 per kg" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=",},
+    {id:"3", product: "Banana", price:"15 per kg" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=",},
+    {id:"4", product: "Orange", price:"17 per kg" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=",}
+
 ]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <div className="title">Vegetables:
         <div className="productRow"></div>
           {
               items.map(item=><Product key={item.id} product={item.product}
                                        price={item.price}
                                        image={item.image}/>)
           }
+          </div>
       </header>
     </div>
   );
--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -4,8 +4,8 @@ const Product=({product,price,image})=>{
     console.log(image);
     return (
         <div className='product-item'>
-            <div className="product__item__description "> {product}</div>
-            <div className="product__item__price"> {price}</div>
+            <div className="product__item__description ">Product: {product}</div>
+            <div className="product__item__price"> Price: {price}</div>
             <img src={image} className="product__item__image" alt={"shir"}/>
         </div>
     );
