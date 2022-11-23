2c14737bf2a041aca0efd88969f17ceb2790018b
liralgazi <liralgazi@gmail.com>
2022-11-23 18:37:18 +0200

    front changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,10 @@ import Product from "./components/Product.js";
 
 
 const items = [
-    {id:"1",desc: "the best red apple", title: "Red Apple", price:"30$ per kg" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=",},
-    {id:"2",desc: "the best green apple", title: "Green Apple", price:"20$ per kg" ,image:"https://media.istockphoto.com/id/625682480/photo/green-apple.jpg?b=1&s=170667a&w=0&k=20&c=hN65bH-I8IEjkNVVmb6rTvxgDrwAbVBB5BTepU-a3-Y=",},
-    {id:"3",desc: "the best banana", title: "Banana", price:"15$ per kg" ,image:"https://media.istockphoto.com/id/1291262112/photo/banana.jpg?b=1&s=170667a&w=0&k=20&c=pPSsFoR1CX7oHJrr-bZPSoFIBO8PPf4SQDg3IbF-wcg=",},
-    {id:"4",desc: "the best orange", title: "Orange", price:"17$ per kg" ,image:"https://media.istockphoto.com/id/185284489/photo/orange.jpg?s=612x612&w=0&k=20&c=m4EXknC74i2aYWCbjxbzZ6EtRaJkdSJNtekh4m1PspE=",}
+    {id:"1",image2: "https://thumbs.dreamstime.com/b/red-apple-half-red-apple-16354511.jpg",desc: "the best red apple", title: "Red Apple", price:"30$ per kg" ,image:"https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?b=1&s=170667a&w=0&k=20&c=aAfiibS9LDxSzOmUvlL1vVIqq7unLny6C6uCnnEh59g=",},
+    {id:"2",image2: "https://thumbs.dreamstime.com/b/fresh-green-apple-green-leaf-cut-half-isolated-white-background-clipping-path-fresh-green-apple-green-leaf-175907435.jpg",desc: "the best green apple", title: "Green Apple", price:"20$ per kg" ,image:"https://media.istockphoto.com/id/625682480/photo/green-apple.jpg?b=1&s=170667a&w=0&k=20&c=hN65bH-I8IEjkNVVmb6rTvxgDrwAbVBB5BTepU-a3-Y=",},
+    {id:"3",image2: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/1200px-Banana-Single.jpg",desc: "the best banana", title: "Banana", price:"15$ per kg" ,image:"https://media.istockphoto.com/id/1291262112/photo/banana.jpg?b=1&s=170667a&w=0&k=20&c=pPSsFoR1CX7oHJrr-bZPSoFIBO8PPf4SQDg3IbF-wcg=",},
+    {id:"4",store:"lala",image2: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Orange-Fruit-Pieces.jpg",desc: "the best orange", title: "Orange", price:"17$ per kg" ,image:"https://media.istockphoto.com/id/185284489/photo/orange.jpg?s=612x612&w=0&k=20&c=m4EXknC74i2aYWCbjxbzZ6EtRaJkdSJNtekh4m1PspE=",}
 
 ]
 
--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -5,9 +5,8 @@ const Product=({product,click,isSelect=false})=>{
         <div className='product-item' onClick={click}>
             <div className="product__item__description ">Product: {product.title}</div>
             <div className="product__item__price"> Price: {product.price}</div>
-            <img src={product.image} className="product__item__image" alt={"shir"}/>
+            <img src={isSelect ? product.image2 : product.image} className="product__item__image" alt={"shir"}/>
             {isSelect && <div className="product__item__desc"> {product.desc}</div>}
-
         </div>
     );
 }
